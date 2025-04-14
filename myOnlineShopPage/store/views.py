c79a0bd9dba0217f358c4b3ea568574ba2816907
Софія Щербатюк <sfchbtik@gmail.com>
2025-04-15 01:26:22 +0300

    Аптейд store->views.py

--- myOnlineShopPage/store/views.py
+++ myOnlineShopPage/store/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def home(request):
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products': products})
+
