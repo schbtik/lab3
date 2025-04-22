7e1ca89cd6294bd2786da93efbde052e4d5ede7e
OlenaSkakun-IPZ-22 <Olena_Skakunn@knu.ua>
2025-04-22 17:24:54 +0300

    заповнвла файл для імпорту

---
+++ myOnlineShopPage/store/views/__init__.py
@@ -0,0 +1,4 @@
+from .cart_views import add_to_cart, cart_view, get_cart,remove_from_cart
+from .home_views import home
+from .product_views import products_view,product_detail
+from .order_views import create_order,order_success
