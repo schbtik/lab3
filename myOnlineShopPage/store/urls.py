07cc6d6ff65b095cf563a396f27edbf8b57823f2
OlenaSkakun-IPZ-22 <sfchbtik@gmail.com>
2025-04-22 17:57:09 +0300

    апдейт urls.py

--- myOnlineShopPage/store/urls.py
+++ myOnlineShopPage/store/urls.py
@@ -12,8 +12,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', cart_view, name='cart_view'),
     path('add-to-cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
-    path('product/', products_view, name='products_view'), 
-    path('product/<int:product_id>/', product_detail, name='product_detail'), 
+    path('product/', products_view, name='products_view'),  
+    path('product/<int:product_id>/', product_detail, name='product_detail'),  
     path('order/', create_order, name='create_order'),  
-    path('order_success/<int:order_id>/', order_success, name='order_success'), 
+    path('order_success/<int:order_id>/', order_success, name='order_success'),  
+    path('remove_from_cart/<int:item_id>/', remove_from_cart, name='remove_from_cart'),
 ]
