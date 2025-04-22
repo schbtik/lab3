65f3e86c715a3300e733687b50b348047614eec5
OlenaSkakun-IPZ-22 <Olena_Skakunn@knu.ua>
2025-04-22 17:24:39 +0300

    видалила непотрібні файли

--- myOnlineShopPage/store/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Product
-
-def home(request):
-    products = Product.objects.all()
-    return render(request, 'store/home.html', {'products': products})
-
