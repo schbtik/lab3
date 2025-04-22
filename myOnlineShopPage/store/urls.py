d7eaeb073d3e9b0783e4b6c509a411b8952c26b0
OlenaSkakun-IPZ-22 <Olena_Skakunn@knu.ua>
2025-04-22 17:35:09 +0300

    додала потрібні url для сторінки home.html

--- myOnlineShopPage/store/urls.py
+++ myOnlineShopPage/store/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from . import views
+from store.views import cart_view, home, products_view
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', home, name='home'),  
+    path('admin/', admin.site.urls),  
+    path('cart/', cart_view, name='cart_view'), 
+    path('product/', products_view, name='products_view'), 
 ]
+
